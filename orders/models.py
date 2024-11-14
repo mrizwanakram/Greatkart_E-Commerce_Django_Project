2670285f600083c496de09743f51660db51ef35f
mrizwanakram786 <mrizwanakramrizwan>
2024-11-15 03:11:10 +0500

    payment method added

--- orders/models.py
+++ orders/models.py
@@ -43,8 +43,15 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def full_address(self):
+        return f"{self.address_line_1}, {self.address_line_2}, {self.city}, {self.state}, {self.country}"
+
+
     def __str__(self):
-        return self.user.first_name
+        return self.first_name
 
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name = 'product_detail'),
     path('search/', views.search, name = 'search'),
 
-]
+] 
