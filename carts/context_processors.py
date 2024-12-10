623b3b973586449c050e1b417a3102b96cdf44d4
mrizwanakram <mrizwanakramrizwan@gmail.com>
2024-12-11 02:34:29 +0500

    code for the store rating and reviews added

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,6 +1,7 @@
 from .models import Cart, CartItem
 from .views import _cart_id
 
+
 def counter(request):
     cart_count = 0
     if 'admin' in request.path:
@@ -11,9 +12,9 @@ def counter(request):
             if request.user.is_authenticated:
                 cart_items = CartItem.objects.all().filter(user=request.user)
             else:
-                cart_items = CartItem.objects.all().filter(cart = cart[:1])
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
-        except CartItem.DoesNotExist:
+        except Cart.DoesNotExist:
             cart_count = 0
-    return dict(cart_count = cart_count)
+    return dict(cart_count=cart_count)
