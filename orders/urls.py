a924c4271d07de2e5488075eb94fa65f463e872c
mrizwanakram <mrizwanakramrizwan@gmail.com>
2025-01-08 03:09:03 +0500

    code for store urls and views updated

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('place_order/', views.place_order, name='place_order'),
-    path('payments/', views.payments, name='payments'),
-    path('order_complete/', views.order_complete, name='order_complete'),
+    path('', views.store, name='store'),
+    path('category/<slug:category_slug>/', views.store, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('search/', views.search, name='search'),
+    path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
 ]
