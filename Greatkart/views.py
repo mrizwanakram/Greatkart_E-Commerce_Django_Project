f88e6c06744617d1ed6744774cb9aa2c6f8b2311
Muhammad Rizwan Akram <mrizwanakramrizwan@gmail.com>
2025-01-22 22:45:35 +0500

    Update views.py

--- Greatkart/views.py
+++ Greatkart/views.py
@@ -4,7 +4,4 @@ from store.models import Product
 def home(request):
     products = Product.objects.all().filter(is_available=True)
 
-    context = {
-        'products': products,
-    }
-    return render(request, 'home.html', context)
+
