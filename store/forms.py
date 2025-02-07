185a8d099576b64344f24feaa32ed210f861b618
Muhammad Rizwan Akram <mrizwanakramrizwan@gmail.com>
2025-02-08 01:50:10 +0500

    Update forms.py

--- store/forms.py
+++ store/forms.py
@@ -1,8 +1,1 @@
-from django import forms
-from .models import ReviewRating
 
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = ReviewRating
-
-        fields = ['subject', 'review', 'rating']
